72c26b8d8ebf28b44d357b64f7ae89c46c402c0b
Matias Singers <mail@matiassingers.com>
2015-01-31 21:51:27 +0800

    Update unit test spec for XML parsing

--- test.js
+++ test.js
@@ -6,9 +6,9 @@ var nock = require('nock');
 
 var klParkingSpots = require('./');
 
-var mocks = nock('http://dbklpgis.my')
-  .get('/PGIS_Mall.js')
-  .reply(200, fs.readFileSync('fixtures/PGIS_Mall.js'));
+var mocks = nock('http://dbklpgis.scadatron.net')
+  .get('/dbklpgisd1.xml')
+  .reply(200, fs.readFileSync('fixtures/dbklpgisd1.xml'));
 
 describe('should format the reponse correctly', function(){
   var spots;
@@ -22,7 +22,7 @@ describe('should format the reponse correctly', function(){
 
   after(function(){
     mocks.done()
-  })
+  });
 
   it('have correct amount of places', function() {
     assert.notEqual(spots, null);
@@ -30,7 +30,12 @@ describe('should format the reponse correctly', function(){
   });
 
   it('have correct properties', function() {
-    assert.equal(spots[2].place, 'Lot10');
-    assert.equal(spots[6].spots, '1598');
+    assert.equal(spots[2].name, 'Lot 10');
+    assert.equal(spots[6].lot, 3498);
+    assert.equal(spots[1].datetime, '31/01/2015 21:45:25');
+    assert.equal(spots[3].healthy, true);
+    assert.equal(spots[4].type, 'Shopping Mall');
+    assert.equal(spots[5].state, 'NA');
+    assert.equal(spots[0].id, 'M1');
   });
 });
